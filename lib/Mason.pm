a096b641f1d8bf71d243f0547e8df988518abdb7
Jonathan Swartz <swartz@pobox.com>
2010-06-16 22:25:06 -0700

    add

---
+++ lib/Mason.pm
@@ -0,0 +1,19 @@
+package Mason;
+
+# Copyright (c) 2010 by Jonathan Swartz. All rights reserved.
+# This program is free software; you can redistribute it and/or modify it
+# under the same terms as Perl itself.
+
+$Mason::VERSION = '0.01';
+
+use Mason::Interp;
+
+1;
+
+__END__
+
+=head1 NAME
+
+Mason - High-performance, dynamic web site authoring system
+
+=cut
---
+++ lib/Mason/Request/TieHandle.pm
@@ -0,0 +1,28 @@
+package Mason::Request::TieHandle;
+use strict;
+use warnings;
+
+sub TIEHANDLE {
+    my $class = shift;
+
+    return bless {}, $class;
+}
+
+sub PRINT {
+    my $self = shift;
+
+    # TODO - why do we need to select STDOUT here?
+    my $old = select STDOUT;
+    $Mason::Request::current_request->print(@_);
+    select $old;
+}
+
+sub PRINTF {
+    my $self = shift;
+
+    # apparently sprintf(@_) won't work, it needs to be a scalar
+    # followed by a list
+    $self->PRINT( sprintf( shift, @_ ) );
+}
+
+1;
