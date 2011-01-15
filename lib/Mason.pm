b48feea2253665d7148875d0d99a6d27bb989ba8
Jonathan Swartz <swartz@pobox.com>
2011-01-14 17:21:24 -0800

    move plugin logic from Mason to Mason::Plugin

--- lib/Mason.pm
+++ lib/Mason.pm
@@ -1,84 +1,32 @@
 package Mason;
 use Carp;
-use Log::Any qw($log);
 use List::Util qw(first);
 use Mason::Interp;
 use Mason::Plugin;
-use Mason::Util qw(can_load);
-use Memoize;
 use Moose;
-use Moose::Meta::Class;
-use MooseX::ClassAttribute;
 use strict;
 use warnings;
 
-sub default_plugins { [] }
-
 sub new {
     my ( $class, %params ) = @_;
-    my $plugins = delete( $params{plugins} ) || [];
-    croak 'plugins must be an array reference' unless ref($plugins) eq 'ARRAY';
-    $plugins = $class->process_plugins( [ @$plugins, @{ $class->default_plugins } ] );
+
+    # Extract plugins and base_interp_class
+    #
+    my $plugins           = delete( $params{plugins} )           || [];
     my $base_interp_class = delete( $params{base_interp_class} ) || 'Mason::Interp';
-    my $interp_class = $class->apply_plugins( $base_interp_class, 'Interp', $plugins );
+
+    # Process plugins and determine real interp_class
+    #
+    $plugins = Mason::Plugin->process_plugins_list($plugins);
+    my $interp_class =
+      Mason::Plugin->apply_plugins_to_class( $base_interp_class, 'Interp', $plugins );
+
+    # Create and return interp
+    #
     return $interp_class->new( mason_root_class => $class, plugins => $plugins, %params );
 }
 
-sub process_plugins {
-    my ( $class, $plugins ) = @_;
-
-    return [ map { $class->process_plugin($_) } @$plugins ];
-}
-
-sub process_plugin {
-    my ( $class, $plugin ) = @_;
-    my @candidates = (
-        substr( $plugin, 0, 1 ) eq '+'
-        ? ( substr( $plugin, 1 ) )
-        : map { join( "::", $_, "Plugin", $plugin ) }
-          ( $class eq 'Mason' ? ($class) : ( $class, 'Mason' ) )
-    );
-    return ( first { can_load($_) } @candidates )
-      || die "could not find plugin '$plugin' in " . join( " or ", @candidates );
-}
-
 my ( %apply_plugins_cache, %final_subclass_seen );
-
-# Apply roles from plugins - adapted from MooseX::Traits
-#
-sub apply_plugins {
-    my ( $class, $base_subclass, $name, $plugins ) = @_;
-    my $subclass;
-
-    my $key = join( ",", $class, $base_subclass, @$plugins );
-    return $apply_plugins_cache{$key} if defined( $apply_plugins_cache{$key} );
-
-    my $final_subclass;
-    my @roles_to_try = map { join( "::", $_, $name ) } @$plugins;
-    if ( $name eq 'Component' ) {
-        push( @roles_to_try, map { join( "::", $_, 'Filters' ) } @$plugins );
-    }
-    my @roles = grep { Class::MOP::is_class_loaded($_) || can_load($_) } @roles_to_try;
-    if (@roles) {
-        my $meta = Moose::Meta::Class->create_anon_class(
-            superclasses => [$base_subclass],
-            roles        => \@roles,
-            cache        => 1
-        );
-        $final_subclass = $meta->name;
-        $meta->add_method( 'meta' => sub { $meta } )
-          if !$final_subclass_seen{$final_subclass}++;
-    }
-    else {
-        $final_subclass = $base_subclass;
-    }
-    $log->debugf( "apply_plugins - base_subclass=%s, name=%s, plugins=%s, roles=%s - %s",
-        $base_subclass, $name, $plugins, \@roles, $final_subclass )
-      if $log->is_debug;
-
-    $apply_plugins_cache{$key} = $final_subclass;
-    return $final_subclass;
-}
 
 1;
 
--- lib/Mason/Plugin.pm
+++ lib/Mason/Plugin.pm
@@ -1,7 +1,60 @@
 package Mason::Plugin;
+use Carp;
+use Log::Any qw($log);
+use Moose;
+use Mason::Moose;
+use Mason::Util qw(can_load);
 use strict;
 use warnings;
 
-# Not sure what goes here yet. All the action is in the plugin subclasses.
+my ( %apply_plugins_cache, %final_subclass_seen );
+
+# CLASS METHODS
+#
+
+method process_plugins_list ($class: $plugins) {
+    croak 'plugins must be an array reference' unless ref($plugins) eq 'ARRAY';
+    $plugins = [ map { $class->process_plugin($_) } @$plugins ];
+}
+
+method process_plugin ($class: $plugin) {
+    my $module =
+        substr( $plugin, 0, 1 ) eq '+' ? ( substr( $plugin, 1 ) )
+      : substr( $plugin, 0, 1 ) eq '@' ? ( "Mason::PluginBundle::" . substr( $plugin, 0, 1 ) )
+      :                                  "Mason::Plugin::$plugin";
+    return can_load($module) ? $module : die "could not load '$module'";
+}
+
+method apply_plugins_to_class ($class: $base_subclass, $name, $plugins) {
+    my $subclass;
+    my $key = join( ",", $base_subclass, @$plugins );
+    return $apply_plugins_cache{$key} if defined( $apply_plugins_cache{$key} );
+
+    my $final_subclass;
+    my @roles_to_try = map { join( "::", $_, $name ) } @$plugins;
+    if ( $name eq 'Component' ) {
+        push( @roles_to_try, map { join( "::", $_, 'Filters' ) } @$plugins );
+    }
+    my @roles = grep { Class::MOP::is_class_loaded($_) || can_load($_) } @roles_to_try;
+    if (@roles) {
+        my $meta = Moose::Meta::Class->create_anon_class(
+            superclasses => [$base_subclass],
+            roles        => \@roles,
+            cache        => 1
+        );
+        $final_subclass = $meta->name;
+        $meta->add_method( 'meta' => sub { $meta } )
+          if !$final_subclass_seen{$final_subclass}++;
+    }
+    else {
+        $final_subclass = $base_subclass;
+    }
+    $log->debugf( "apply_plugins - base_subclass=%s, name=%s, plugins=%s, roles=%s - %s",
+        $base_subclass, $name, $plugins, \@roles, $final_subclass )
+      if $log->is_debug;
+
+    $apply_plugins_cache{$key} = $final_subclass;
+    return $final_subclass;
+}
 
 1;
