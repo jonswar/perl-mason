0fda06d35e7b4e286c05e75ba08b90fccc6b3980
Jonathan Swartz <swartz@pobox.com>
2011-01-16 11:53:31 -0800

    update skel

--- lib/Mason/t/Skel.pm
+++ lib/Mason/t/Skel.pm
@@ -4,10 +4,10 @@ use Test::Class::Most parent => 'Mason::Test::Class';
 sub test_ : Test(1) {
     my $self = shift;
     $self->test_comp(
-        src => <<'EOF',
-EOF
-        expect => <<'EOF',
-EOF
+        src => '
+',
+        expect => '
+',
     );
 }
 
