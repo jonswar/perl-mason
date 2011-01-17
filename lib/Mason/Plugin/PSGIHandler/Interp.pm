a52754fef9c80e539b24ee8bebbea803d930dc0f
Jonathan Swartz <swartz@pobox.com>
2011-01-17 06:40:01 -0800

    move comp_path and parameters into Interp, a little more consolidated

--- lib/Mason/Plugin/PSGIHandler/Interp.pm
+++ lib/Mason/Plugin/PSGIHandler/Interp.pm
@@ -4,11 +4,22 @@ use Method::Signatures::Simple;
 use Moose::Role;
 
 method handle_psgi ($env) {
-    my $req      = Mason::Plugin::PSGIHandler::PlackRequest->new($env);
-    my $result   = $self->run( { req => $req }, $req->mason_comp_path, $req->mason_parameters );
+    my $req = Mason::Plugin::PSGIHandler::PlackRequest->new($env);
+    my $result =
+      $self->run( { req => $req }, $self->psgi_comp_path($req), $self->psgi_parameters($req) );
     my $response = $result->plack_response;
     $response->content( $result->output );
     return $response->finalize;
+}
+
+method psgi_comp_path ($req) {
+    my $comp_path = $req->path;
+    $comp_path = "/$comp_path" if substr( $comp_path, 0, 1 ) ne '/';
+    return $comp_path;
+}
+
+method psgi_parameters ($req) {
+    return $req->parameters;
 }
 
 1;
