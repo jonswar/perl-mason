c78ba99ede34843aa12b62fbbbb096489a2a62fe
Jonathan Swartz <swartz@pobox.com>
2011-01-18 14:59:09 -0800

    eliminated deprecated Moose->import({into => ...}) per doy

--- lib/Mason/Moose.pm
+++ lib/Mason/Moose.pm
@@ -5,15 +5,17 @@ use MooseX::StrictConstructor  ();
 use Method::Signatures::Simple ();
 use namespace::autoclean       ();
 use Moose::Exporter;
-Moose::Exporter->setup_import_methods(
-    also => [ 'MooseX::HasDefaults::RO', 'MooseX::StrictConstructor' ] );
+Moose::Exporter->setup_import_methods( also => ['Moose'] );
 
 sub init_meta {
-    my ( $class, %params ) = @_;
+    my $class     = shift;
+    my %params    = @_;
     my $for_class = $params{for_class};
     Method::Signatures::Simple->import( into => $for_class );
     namespace::autoclean->import( -cleanee => $for_class );
-    Moose->import( { into => $for_class } );
+    Moose->init_meta(@_);
+    MooseX::StrictConstructor->init_meta(@_);
+    MooseX::HasDefaults::RO->init_meta(@_);
 }
 
 1;
