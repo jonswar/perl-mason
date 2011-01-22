2ec18ea8c5176093bb3380bed35363f24f8a2d4a
Jonathan Swartz <swartz@pobox.com>
2011-01-22 02:37:01 -0800

    implement accept_path_info

--- lib/Mason/t/AdvancedPageResolution.pm
+++ lib/Mason/t/AdvancedPageResolution.pm
@@ -3,7 +3,7 @@ use Test::Class::Most parent => 'Mason::Test::Class';
 
 __PACKAGE__->default_plugins( [ '@Default', 'AdvancedPageResolution' ] );
 
-sub test_resolve : Tests(19) {
+sub test_resolve : Tests(22) {
     my $self = shift;
 
     my $try = sub {
@@ -12,9 +12,16 @@ sub test_resolve : Tests(19) {
 
         $self->setup_dirs;
         foreach my $existing_path (@$existing_paths) {
+            my $accept_path_info = 0;
+            if ( $existing_path =~ /=1$/ ) {
+                substr( $existing_path, -2, 2 ) = '';
+                $accept_path_info = 1;
+            }
             $self->add_comp(
                 path => $existing_path,
-                src  => 'path: <% $self->cmeta->path %>; path_info: <% $m->path_info %>'
+                src  => join( "",
+                    ( $accept_path_info ? "%% sub accept_path_info { $accept_path_info }\n" : "" ),
+                    "path: <% \$self->cmeta->path %>; path_info: <% \$m->path_info %>" )
             );
         }
         my $desc = sprintf( "run %s against %s", $run_path, join( ",", @$existing_paths ) );
@@ -33,15 +40,18 @@ sub test_resolve : Tests(19) {
     $try->( $run_path, ['/foo/bar/baz.m'],          '/foo/bar/baz.m',          '' );
     $try->( $run_path, ['/foo/bar/baz/dhandler.m'], '/foo/bar/baz/dhandler.m', '' );
     $try->( $run_path, ['/foo/bar/baz/index.m'],    '/foo/bar/baz/index.m',    '' );
-    $try->( $run_path, ['/foo/bar.m'],              '/foo/bar.m',              'baz' );
+    $try->( $run_path, ['/foo/bar.m=1'],            '/foo/bar.m',              'baz' );
     $try->( $run_path, ['/foo/bar/dhandler.m'],     '/foo/bar/dhandler.m',     'baz' );
-    $try->( $run_path, ['/foo.m'],                  '/foo.m',                  'bar/baz' );
+    $try->( $run_path, ['/foo.m=1'],                '/foo.m',                  'bar/baz' );
     $try->( $run_path, ['/foo/dhandler.m'],         '/foo/dhandler.m',         'bar/baz' );
     $try->( $run_path, ['/dhandler.m'],             '/dhandler.m',             'foo/bar/baz' );
     $try->( $run_path, [ '/dhandler.m',     '/foo/dhandler.m' ], '/foo/dhandler.m', 'bar/baz' );
-    $try->( $run_path, [ '/foo/dhandler.m', '/foo/bar.m' ],      '/foo/bar.m',      'baz' );
+    $try->( $run_path, [ '/foo/dhandler.m', '/foo/bar.m=1' ],    '/foo/bar.m',      'baz' );
+    $try->( $run_path, [ '/foo/dhandler.m', '/foo/bar.m' ],      '/foo/dhandler.m', 'bar/baz' );
 
     # Not found
+    $try->( $run_path, ['/foo/bar.m'],                    undef );
+    $try->( $run_path, ['/foo.m'],                        undef );
     $try->( $run_path, ['/foo/bar/baz/blarg.m'],          undef );
     $try->( $run_path, ['/foo/bar/baz/blarg/dhandler.m'], undef );
     $try->( $run_path, ['/foo/bar/baz'],                  undef );
@@ -77,6 +87,7 @@ sub test_decline : Tests(7) {
                 path => $existing_path,
                 src  => $component,
             );
+            $self->add_comp( path => '/Base.pm', src => 'sub accept_path_info { 1 }' );
         }
         my $desc = sprintf( "declining: %s", join( ",", @paths_to_decline ) );
         if ( defined($resolve_path) ) {
