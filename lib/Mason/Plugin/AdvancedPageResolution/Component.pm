8473e32ad40049f4de5ef46ed477bb7ede28f55b
Jonathan Swartz <swartz@pobox.com>
2011-01-28 22:12:04 -0800

    fixes

--- lib/Mason/Plugin/AdvancedPageResolution/Component.pm
+++ lib/Mason/Plugin/AdvancedPageResolution/Component.pm
@@ -1,12 +1,11 @@
 package Mason::Plugin::AdvancedPageResolution::Component;
 use Mason::PluginRole;
 
-method is_dhandler  () {
+method is_dhandler () {
     return grep { $self->cmeta->name eq $_ } @{ $self->m->interp->dhandler_names };
 }
 
 method accept () {
-    $DB::single = 1;
     my $m = $self->m;
     if ( $m->has_path_info && !$m->path_info_accessed && !$self->is_dhandler ) {
         $m->decline( "got path_info - " . $m->path_info );
