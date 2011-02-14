d794281f08589768ad7bc3303ca0065cae3d22f7
Jonathan Swartz <swartz@pobox.com>
2011-02-14 01:27:29 -0800

    add

---
+++ lib/Mason/Moose/Role.pm
@@ -0,0 +1,36 @@
+package Mason::Moose::Role;
+use Moose::Role                ();
+use Method::Signatures::Simple ();
+use namespace::autoclean       ();
+use Moose::Exporter;
+Moose::Exporter->setup_import_methods( also => ['Moose::Role'] );
+
+sub init_meta {
+    my $class     = shift;
+    my %params    = @_;
+    my $for_class = $params{for_class};
+    Method::Signatures::Simple->import( into => $for_class );
+    namespace::autoclean->import( -cleanee => $for_class );
+    Moose::Role->init_meta(@_);
+}
+
+1;
+
+# ABSTRACT: Mason Moose role policies
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    # instead of use Moose::Role;
+    use Mason::Moose::Role;
+
+=head1 DESCRIPTION
+
+Sets certain Moose behaviors for Mason's internal roles. Using this module is
+equivalent to
+
+    use Moose::Role;
+    use Method::Signatures::Simple;
+    use namespace::autoclean;
