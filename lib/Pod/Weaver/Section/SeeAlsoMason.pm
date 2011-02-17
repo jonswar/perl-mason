199cfa83b94a0b8bb81eb5d7bbd6553707be4ca8
Jonathan Swartz <swartz@pobox.com>
2011-02-16 16:48:34 -0800

    don't put see also if already there

--- lib/Pod/Weaver/Section/SeeAlsoMason.pm
+++ lib/Pod/Weaver/Section/SeeAlsoMason.pm
@@ -9,6 +9,12 @@ use Moose::Autobox;
 sub weave_section {
     my ( $self, $document, $input ) = @_;
 
+    my $idc = $input->{pod_document}->children;
+    for ( my $i = 0 ; $i < $idc->length ; $i++ ) {
+        next unless my $para = $idc->[$i];
+        return
+          if $para->can('command') && $para->command eq 'head1' && $para->content eq 'SEE ALSO';
+    }
     $document->children->push(
         Pod::Elemental::Element::Nested->new(
             {
