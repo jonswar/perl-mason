dadd93b50d880a522a84e406496dd4f565bba647
Jonathan Swartz <swartz@pobox.com>
2011-02-26 17:13:38 -0800

    add comment

--- lib/Mason/Plugin/DollarDot/Compilation.pm
+++ lib/Mason/Plugin/DollarDot/Compilation.pm
@@ -3,6 +3,8 @@ use Mason::PluginRole;
 
 after 'process_perl_code' => sub {
     my ( $self, $coderef ) = @_;
+
+    # Replace $. with $self->
     $$coderef =~ s/ \$\.([^\W\d]\w*) / \$self->$1 /gx;
 };
 
