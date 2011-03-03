f98c7a96b4474a4ebbbc7698c99f5f93c19f889c
Jonathan Swartz <swartz@pobox.com>
2011-03-02 16:52:43 -0800

    Move Mason->new parameters to Mason/Manual/Parameters.pod

--- lib/Mason.pm
+++ lib/Mason.pm
@@ -35,8 +35,14 @@ Mason - Powerful, high-performance templating for the web and beyond
 
 =head1 SYNOPSIS
 
+  foo.m:
     % my $name = "Mason";
     Hello world! Welcome to <% $name %>.
+
+  #!/usr/local/bin/perl
+  use Mason;
+  my $mason = Mason->new(comp_root => '...');
+  print $mason->run('/foo')->output;
 
 =head1 DESCRIPTION
 
