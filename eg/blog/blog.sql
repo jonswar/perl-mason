133d315ac523b831ea5cd61e28ee296b20cd4f5d
Jonathan Swartz <swartz@pobox.com>
2012-05-05 16:24:17 -0700

    delete blog (moved to Poet)

--- eg/blog/blog.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists articles (
-  id integer primary key autoincrement,
-  content string not null,
-  create_time integer not null,
-  title string not null
-);
--- eg/blog/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Requires cpanm: http://search.cpan.org/perldoc?App::cpanminus
-cpanm -S Date::Format DBD::SQLite Mason Mason::Plugin::PSGIHandler Mason::Plugin::HTMLFilters
-cpanm -S Plack Plack::Middleware::Session Rose::DB::Object
-
-mkdir -p data
-sqlite3 data/blog.db < blog.sql
