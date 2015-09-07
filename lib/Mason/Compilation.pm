d6a046ce0905611f8fdbe31ee738809eba78ed5b
Paul Cochrane <paul@liekut.de>
2015-09-07 11:40:42 +0200

    Update copyright year

--- lib/Mason/Compilation.pm
+++ lib/Mason/Compilation.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 1998-2005 by Jonathan Swartz. All rights reserved.
+# Copyright (c) 1998-2015 by Jonathan Swartz. All rights reserved.
 # This program is free software; you can redistribute it and/or modify it
 # under the same terms as Perl itself.
 
